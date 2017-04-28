6692c0a117db23e00e67aaa3b8604450159144c0
javier <javi.bielsa1995@gmail.com>
2017-04-28 09:39:20 +0200

    Primera parte

---
+++ Black_jack/Carta.java
@@ -0,0 +1,22 @@
+package Black_jack;
+import java.awt.*;
+import java.applet.*;
+public class Carta {
+	Image imagen;
+	int valor;
+	int posx, posy;
+	
+	public Carta(Image img,int v) {
+		imagen=img;
+		valor=(v>10)?10:v;
+	}
+	public void dibujar(Graphics g, Applet a){
+		g.drawImage(imagen, posx, posy, 100, 200,a);
+	}
+	public void setPosx(int px){
+		posx=px;
+	}
+	public void setPosy(int py){
+		posy=py;
+	}
+}
